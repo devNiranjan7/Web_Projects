379cbebfabb8fa45d24eaa2133acfeb512bf84b4
Dev Niranjan <devniranjan0111@gmail.com>
2025-06-28 15:05:38 +0530

    updated ps6

--- #JS/ps6.js
+++ #JS/ps6.js
@@ -6,19 +6,40 @@ age = prompt("Enter your age");
 
 age = Number(age);
 
-(age>=18)?alert("You can drive"):alert("You can't drive");
+// (age >= 18) ? alert("You can drive") : alert("You can't drive");
 
 // Question 2
 
+let isTrue = confirm("Want to see the prompt again.");
 
+if (isTrue) {
+    age = prompt("Enter your age");
+}
+
+// (age >= 18) ? alert("You can drive") : alert("You can't drive");
 
 // Question 3
 
+if (age < 0) {
+    console.error("Age entered is negative!")
+}
 
+else {
+    (age >= 18) ? alert("You can drive") : alert("You can't drive");
+}
 
 // Question 4
 
+let num;
 
+num = prompt("Enter number : ");
+
+if (Number(num) > 4) {
+    location.href = "https://www.google.com";
+}
 
 // Question 5
 
+let bgcolor = prompt("Enter the color of background : ");
+
+document.body.style.backgroundColor = bgcolor;
